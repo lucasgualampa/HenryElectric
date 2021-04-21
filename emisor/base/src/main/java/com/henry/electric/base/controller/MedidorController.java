7d473ebc610cec149ee62e2d3dea4cf67f28b864
lucasgualampa <g.brianlucas1993@gmail.com>
2021-04-21 11:19:00 -0300

    crud emisor

--- emisor/base/src/main/java/com/henry/electric/base/controller/MedidorController.java
+++ emisor/base/src/main/java/com/henry/electric/base/controller/MedidorController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("medidores")
+@RequestMapping("mediciones")
 public class MedidorController {
 
     @Autowired
